d02a0af256df48d2b5152f94b7cce4080439c1e0
JOJO <nwin919191@gmail.com>
2025-07-27 20:11:30 +0900

    implement login, request new password, reset new password page

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,18 +1,31 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
+import RequestPassword from "../views/RequestPassword.vue";
+import ResetPassword from "../views/ResetPassword.vue";
 
 const routes = [
     {
         path: '/dashboard',
-        name: 'Dashboard',
+        name: 'dashboard',
         component: Dashboard
     },
     {
         path: '/login',
-        name: 'Login',
+        name: 'login',
         component: Login
     },
+    {
+        path: '/request-password',
+        name: 'requestPassword',
+        component: RequestPassword
+    },
+    {
+        path: '/reset-password/:token',
+        name: 'resetPassword',
+        component: ResetPassword
+    },
+
 ];
 
 
