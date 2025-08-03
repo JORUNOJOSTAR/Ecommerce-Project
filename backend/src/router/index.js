1a02c6417b00171b82d33a5bcae24d4a71a40e19
JOJO <nwin919191@gmail.com>
2025-08-03 20:17:24 +0900

    manage guest and auth routes with meta and user store

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -7,12 +7,16 @@ import AppLayout from "../components/AppLayout.vue";
 import Products from "../views/Products.vue";
 import Reports from "../views/Reports.vue";
 import Users from "../views/Users.vue";
+import store from "../store/index.js";
 
 const routes = [
     {
         path: '/app',
         name: 'app',
         component: AppLayout,
+        meta: {
+          requiresAuth: true,
+        },
         children:[
             {
                 path: 'dashboard',
@@ -39,17 +43,26 @@ const routes = [
     {
         path: '/login',
         name: 'login',
-        component: Login
+        component: Login,
+        meta: {
+            requiresGuest: true,
+        }
     },
     {
         path: '/request-password',
         name: 'requestPassword',
-        component: RequestPassword
+        component: RequestPassword,
+        meta: {
+            requiresGuest: true,
+        }
     },
     {
         path: '/reset-password/:token',
         name: 'resetPassword',
-        component: ResetPassword
+        component: ResetPassword,
+        meta: {
+            requiresGuest: true,
+        }
     },
 
 ];
@@ -58,6 +71,16 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
+});
+
+router.beforeEach((to,from,next)=>{
+    if(to.meta.requiresAuth && !store.state.user.token){
+        next({name:'login'});
+    }else if(to.meta.requiresGuest && store.state.user.token){
+        next({name: 'app.dashboard'});
+    }else{
+        next();
+    }
 });
 
 export default router;
