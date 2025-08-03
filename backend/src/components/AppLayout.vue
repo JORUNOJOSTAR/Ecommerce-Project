0f4b36596a93a5ee6b132045248cdce1fb8a8e03
JOJO <nwin919191@gmail.com>
2025-08-03 19:11:35 +0900

    implement sidebar toggle button

--- backend/src/components/AppLayout.vue
+++ backend/src/components/AppLayout.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="min-h-full flex bg-gray-200">
         <!--        Sidebar-->
-        <Sidebar/>
+        <Sidebar :class="{'-ml-[200px]': !sideBarOpened}"/>
         <!--        Sidebar-->
         <div class="flex-1 ">
-            <TopHeader/>
+            <TopHeader @toggle-sidebar="toggleSidebar"/>
 <!--            Content-->
             <main class="p-6">
                 <router-view></router-view>
@@ -19,7 +19,13 @@
 <script setup>
 import Sidebar from "./Sidebar.vue";
 import TopHeader from "./TopHeader.vue";
+import {ref} from "vue";
 
+const sideBarOpened = ref(true);
+function toggleSidebar() {
+    sideBarOpened.value = !sideBarOpened.value;
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
