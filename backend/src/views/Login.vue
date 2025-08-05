afa649c0bb9da0dffe738cc03fd830282ff93d20
JOJO <nwin919191@gmail.com>
2025-08-05 20:50:56 +0900

    add remember me checkbox to login form

--- backend/src/views/Login.vue
+++ backend/src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <GuestLayout title="Sign in to your account">
-            <form class="space-y-6" action="#" method="POST">
+            <form class="space-y-3" action="#" method="POST">
                 <div>
                     <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
                     <div class="mt-2">
@@ -17,11 +17,18 @@
                         </div>
                     </div>
                     <div class="mt-2">
-                        <input placeholder="Signin Password" type="password" name="password" id="password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                        <input placeholder="Sign in Password" type="password" name="password" id="password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                    </div>
+
+                    <div class="pt-2 flex items-center">
+                        <input id="remember-me" name="remember-me" type="checkbox"
+                               class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"/>
+                        <label for="remember-me" class="ml-2 block text-sm font-medium text-gray-500"> Remember me </label>
                     </div>
                 </div>
 
-                <div>
+
+                <div class="pt-3">
                     <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
                 </div>
             </form>
